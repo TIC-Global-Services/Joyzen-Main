2574d68e031af65f4afd3246322fa68414eadc34
praveen <praveenramesh5267@gmail.com>
2026-09-17 14:41:36 +0530

    loader update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import WaveGridBackground from "@/reuseable/wave-grid-background";
 import { epilogue, satoshi } from "@/fonts/font";
 import Navbar from "@/reuseable/Navbar";
 import Footer from "@/reuseable/Footer";
+import Preloader from "@/reuseable/loader";
 
 
 
@@ -24,6 +25,7 @@ export default function RootLayout({
       className={`${satoshi.variable} ${epilogue.variable} font-sans h-full antialiased`}
     >
       <body className="min-h-full flex flex-col relative bg-white text-foreground font-sans">
+        <Preloader />
         {/* Full-site 3D Honeycomb background with localized #EF8F60 border glow */}
         <div className="fixed inset-0 -z-10 pointer-events-none overflow-hidden select-none">
           <WaveGridBackground colorBase="#ffffff" colorHigh="#EF8F60" autoAnimate={false} waveAmplitude={0.5} waveMaxHeight={0.5} />
